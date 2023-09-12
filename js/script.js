53ca0e6988ef9b467495724924ba3b51b3ab5405
Idedav <xdavide.correa@gmail.com>
2023-09-12 15:56:04 +0200

    [add] dice game on js on html

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const emailsAdded = [
 ]
 const emailUser = prompt('Inserisci la tua email per la registrazione al gioco.')
 let emailWrong = false
-let message;
+let messageEmail;
 // EMAIL'S CONTROL
 for(let i= 0; i < emailsAdded.length; i++){
     const email = emailsAdded[i];
@@ -15,29 +15,33 @@ for(let i= 0; i < emailsAdded.length; i++){
     if(email === emailUser){
         emailWrong = true
         console.log('wrong email');
-        message = 'Utente già registrato'
+        messageEmail = 'Utente già registrato'
     }
 
 }
     if(!emailWrong){
     emailsAdded.push(emailUser)
-    message = 'Benvenuto!'
+    messageEmail = 'Benvenuto!'
 // DICES'S GAME
     const btnDice = document.getElementById('btn-dice')
     btnDice.addEventListener('click', function(){
 // NUMBERS GENERATOR
         const userDice = Math.floor((Math.random() * 6) + 1);
         const pcDice = Math.floor((Math.random() * 6) + 1);
+        let messageDice;
         console.log(userDice, pcDice)
 // NUMBERS CONTROL
         if(userDice > pcDice){
             console.log('Hai vinto')
+            messageDice = 'Ti è uscito ' + userDice + ' e al pc è uscito ' + pcDice + ' Hai vinto!'
         }else if(userDice < pcDice){
             console.log('Hai perso')
+            messageDice = 'Ti è uscito ' + userDice + ' e al pc è uscito ' + pcDice + ' Hai perso...'
         }else if(userDice == pcDice){
             console.log('pareggio')
+            messageDice = 'Ti è uscito ' + userDice + ' e al pc è uscito ' + pcDice + ' Hai pareggiato.'
         }
+        document.getElementById('output-dice').innerHTML = messageDice
     })
     }
-
-    document.getElementById('output').innerHTML = message
+    document.getElementById('output-email').innerHTML = messageEmail
